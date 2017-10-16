271ff124929763a4dd52ca7314ffe51933ca86aa
Sejuapig <sejuapig@gmail.com>
2017-10-16 15:18:26 +0200

    dev createUser

---
+++ src/application.js
@@ -0,0 +1,5 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+var CreateUser = require("./components/CreateUser");
+
+ReactDOM.render(<CreateUser/>, document.getElementById("main"));
